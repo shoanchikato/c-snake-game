3a7bd3ab9702f16ba3b72784efbea34c854b5c1d
Shoan Chikato <sjchikato@gmail.com>
2024-10-05 17:20:12 +0200

    implement resize ratio

--- constants.h
+++ constants.h
@@ -3,13 +3,13 @@
 
 #include "include/raylib.h"
 
-#define CELLSIZE 30
+#define CELLSIZE (30 / RESIZERATIO)
 #define CELLCOUNT 25
 
 #define HEIGHT (CELLCOUNT * CELLSIZE)
 #define WIDTH (CELLCOUNT * CELLSIZE)
 
-#define RESIZERATIO 1
+#define RESIZERATIO 4
 
 #define GREEN_N CLITERAL(Color){174, 204, 96, 255}
 #define DARKGREEN_N CLITERAL(Color){43, 51, 24, 255}
